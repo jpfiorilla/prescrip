07aad7f44fb83f11ade291ece79d31ed7c384404
audiosprite <jpfiorilla@gmail.com>
2017-01-02 09:47:59 -0500

    before test

--- README.md
+++ README.md
@@ -2,5 +2,7 @@ Steps
 
 1. Install Node.js (https://nodejs.org/en/download/)
 2. $ npm install npm@latest -g
-3. $ node index (if this yields ReferenceError, enter `$ npm install`)
-4. 
+3. $ node index (if this yields ReferenceError, enter $ `npm install`)
+4. Enter an input file name, eg >`input2.txt`
+5. Your output.txt file will be in the same folder as index.js
+6. Ctrl+c when finished to terminate
--- index.js
+++ index.js
@@ -21,105 +21,117 @@ const write = function(filename){
         });
 }
 
-const filename = 'input2.txt';
-
 // parses input txt file
 const elevatorProblem = function(filename){
-// const filename = 'input2.txt';
-const input = require('./input-files/' + filename);
-const inputArr = input.split("\n");
-if (inputArr.length !== 4){
-    console.error('Invalid input; must have an M, N, Q, and array of people; input contains ' + inputArr.length + ' parameters');
-    process.exit();
-}
-const numElevators = Number(inputArr[0]);
-const floors = Number(inputArr[1]);
-const capacity = Number(inputArr[2]);
-const queue = inputArr[3].split(',');
-for (var i = 0; i < queue.length; i++){
-    queue[i] = Number(queue[i])
-    if (queue[i] > floors){
-        console.error('Invalid input; person ' + (i + 1).toString() + ' is attempting to go to floor ' + queue[i].toString() + ', but only ' + floors.toString() + ' floors exist.');
+    const input = require('./input-files/' + filename);
+    const inputArr = input.split("\n");
+    if (inputArr.length !== 4){
+        console.error('Invalid input; must have an M, N, Q, and array of people; input contains ' + inputArr.length + ' parameters');
         process.exit();
     }
-};
-
-// initializes elevator system
-let bottomFloorElevators = [], log = [], elevators = [], passengersInTransit = capacity * numElevators;
-for (var i = 0; i < numElevators; i++){
-    elevators.push({id: i,
-        position: 0,
-        ascending: true,
-        passengers: [],
-        targetFloor: floors
-    });
-    bottomFloorElevators.push(i);
-}
-
-// runs simulation
-i = 0; // i = time in seconds
-let consolelog = '';
-while (queue.length + passengersInTransit > 0){
-    log[i] = 'T(' + i + '): ';
-
-    // find available elevators on floor 0
-    bottomFloorElevators = [];
-    for (var j = 0; j < elevators.length; j++){
-        if (elevators[j].position === 0) {
-            bottomFloorElevators.push(j);
-            elevators[j].ascending = true;
-        } else if (elevators[j].position >= elevators[j].targetFloor){
-            elevators[j].ascending = false;
+    const numElevators = Number(inputArr[0]);
+    const floors = Number(inputArr[1]);
+    const capacity = Number(inputArr[2]);
+    const queue = inputArr[3].split(',');
+    for (var i = 0; i < queue.length; i++){
+        queue[i] = Number(queue[i])
+        if (queue[i] > floors){
+            console.error('Invalid input; person ' + (i + 1).toString() + ' is attempting to go to floor ' + queue[i].toString() + ', but only ' + floors.toString() + ' floors exist.');
+            process.exit();
         }
-    }
+    };
 
-    //  elevators receive passengers; log is populated as passengers board
-    let currentPool = queue.slice(0,  bottomFloorElevators.length * capacity).sort();
-    for (j = 0; j < bottomFloorElevators.length; j++){
-        let currentElevator = elevators[bottomFloorElevators[j]];
-        currentElevator.passengers = currentPool.slice(j*capacity, j*capacity + capacity);
-        if (currentElevator.passengers.length) currentElevator.targetFloor = currentElevator.passengers.max();
-        currentElevator.passengers.length > 0 ?
-        log[i] += currentElevator.passengers.length + ' passengers board elevator ' + currentElevator.id + '. ' :
-        log[i] += 'Elevator ' + currentElevator.id + ' returns to the bottom floor. ';
-        for (var k = 0; k < capacity; k++) queue.shift();
-    }
-
-    // elevators move up & down; log is populated as passengers leave
-    passengersInTransit = 0;
-    for (j = 0; j < elevators.length; j++){
-        currentElevator = elevators[j];
-        if (currentElevator.ascending){
-            currentElevator.position += 0.5;
-            let departingPassengers = 0;
-            while (currentElevator.position === currentElevator.passengers.min()){
-                departingPassengers++;
-                currentElevator.passengers.splice(currentElevator.passengers.indexOf(currentElevator.passengers.min()), 1);
-            }
-            if (departingPassengers){
-                departingPassengers === 1 ? 
-                log[i] += departingPassengers + ' passenger exits elevator ' + currentElevator.id + ' on floor ' + currentElevator.position + '. ' :
-                log[i] += departingPassengers + ' passengers exit elevator ' + currentElevator.id + ' on floor ' + currentElevator.position + '. ';
-            }
-        passengersInTransit += currentElevator.passengers.length;
-        } else currentElevator.position--;
-    }
-
-    // console.log('current pool: ', currentPool, ' bottom floor elevators: ', bottomFloorElevators, ' elevators: ', elevators, ' log: ', log[i], ' in transit: ', passengersInTransit, ' i: ', i);
-    i++;
-}
-fs.writeFile('./output.txt', filename + ' elevator logs\n' + log.join('\n'), function(err){
-            if(err) return console.log(err);
-            console.log('Result written to ./output.txt');
+    // initializes elevator system
+    let bottomFloorElevators = [], log = [], elevators = [], passengersInTransit = capacity * numElevators;
+    for (var i = 0; i < numElevators; i++){
+        elevators.push({id: i,
+            position: 0,
+            ascending: true,
+            passengers: [],
+            targetFloor: floors
         });
+        bottomFloorElevators.push(i);
+    }
+
+    // runs simulation
+    i = 0; // i = time in seconds
+    while (queue.length + passengersInTransit > 0){
+        log[i] = 'T(' + i + '): ';
+
+        // find available elevators on floor 0
+        bottomFloorElevators = [];
+        for (var j = 0; j < elevators.length; j++){
+            if (elevators[j].position === 0) {
+                bottomFloorElevators.push(j);
+                elevators[j].ascending = true;
+            } else if (elevators[j].position >= elevators[j].targetFloor){
+                elevators[j].ascending = false;
+            }
+        }
+
+        //  elevators receive passengers; log is populated as passengers board
+        let currentPool = queue.slice(0,  bottomFloorElevators.length * capacity).sort();
+        for (j = 0; j < bottomFloorElevators.length; j++){
+            let currentElevator = elevators[bottomFloorElevators[j]];
+            currentElevator.passengers = currentPool.slice(j*capacity, j*capacity + capacity);
+            if (currentElevator.passengers.length) currentElevator.targetFloor = currentElevator.passengers.max();
+            currentElevator.passengers.length > 0 ?
+            log[i] += currentElevator.passengers.length + ' passengers board elevator ' + currentElevator.id + '. ' :
+            log[i] += 'Elevator ' + currentElevator.id + ' returns to the bottom floor. ';
+            for (var k = 0; k < capacity; k++) queue.shift();
+        }
+
+        // elevators move up & down; log is populated as passengers leave
+        passengersInTransit = 0;
+        for (j = 0; j < elevators.length; j++){
+            currentElevator = elevators[j];
+            if (currentElevator.ascending){
+                currentElevator.position += 0.5;
+                let departingPassengers = 0;
+                while (currentElevator.position === currentElevator.passengers.min()){
+                    departingPassengers++;
+                    currentElevator.passengers.splice(currentElevator.passengers.indexOf(currentElevator.passengers.min()), 1);
+                }
+                if (departingPassengers){
+                    departingPassengers === 1 ? 
+                    log[i] += departingPassengers + ' passenger exits elevator ' + currentElevator.id + ' on floor ' + currentElevator.position + '. ' :
+                    log[i] += departingPassengers + ' passengers exit elevator ' + currentElevator.id + ' on floor ' + currentElevator.position + '. ';
+                }
+            passengersInTransit += currentElevator.passengers.length;
+            } else currentElevator.position--;
+        }
+
+        console.log('current pool: ', currentPool, ' bottom floor elevators: ', bottomFloorElevators, ' elevators: ', elevators, ' log: ', log[i], ' in transit: ', passengersInTransit, ' i: ', i);
+        i++;
+    }
+    let firstline = filename + ' elevator logs\n' + 'Total time to serve all passengers: ' + (i/60).toFixed(0) + ' minutes and ' + (i%60) + ' seconds.\n'
+    fs.writeFile('./output.txt', firstline + log.join('\n'), function(err){
+                if(err) return console.log(err);
+                console.log('Result written to ./output.txt');
+            });
 }
 
-elevatorProblem(filename);
+// elevatorProblem(filename);
 
 // allows node to take input from the terminal
 const readline = require('readline');
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-//   prompt: 'Enter a file name> '
-// });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'Enter a file name> '
+});
+rl.prompt();
+rl.on('line', (line) => {
+  switch(line.trim()) {
+    case 'hello':
+      console.log('world!');
+      break;
+    default:
+      elevatorProblem(line.trim());
+      break;
+  }
+  rl.prompt();
+}).on('close', () => {
+  console.log('\nHave a great day!');
+  process.exit(0);
+});
